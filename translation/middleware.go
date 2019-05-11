c3a8767733e73c7697e703150744a67e168a98b2
Aleksei Piianin <piyanin@gmail.com>
2019-05-12 01:39:25 +0300

    Add a middleware for storing a translation context created from HTTP request into a gin context

---
+++ translation/middleware.go
@@ -0,0 +1,29 @@
+package translation
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/microparts/i18n-go/translation"
+)
+
+const (
+	ContextParam = "translation-context"
+)
+
+func Header(base translation.Context) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Set(ContextParam, translation.NewContext(base, c.Request))
+	}
+}
+
+func ContextFromGin(base translation.Context, c *gin.Context) translation.Context {
+	v, ok := c.Get(ContextParam)
+	if !ok {
+		return base
+	}
+	result, ok := v.(translation.Context)
+	if !ok {
+		return base
+	}
+
+	return result
+}
